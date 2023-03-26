72cc0ed3ba74acd252c5e84a4760cda70cec33e6
Max <maxf7897@gmail.com>
2023-03-26 09:14:21 +0300

    sec com

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -5,7 +5,7 @@ int main()
 	printf("Fedorov Maksym grupa KNMS-11 pidgrupa 1 lab 2 task 2 \n");
 	float suma;
 	printf("Vvedit sumu groshey v grn: ");
-	scanf_s("%f", &suma);
+	scanf("%f", &suma);
 	float eur = 39.07;
 	float usd = 36.85;
 	eur = suma / eur;
